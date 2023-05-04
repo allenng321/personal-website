2c2171bca0c3afb9454b3fcfa6d32c3056a9f782
Allen <pikachuexeallen@gmail.com>
2023-05-04 00:35:15 -0400

    Init NextJs Project

---
+++ src/pages/_document.tsx
@@ -0,0 +1,14 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head />
+      <body>
+        <Main />
+        <NextScript />
+        <h1>Footer</h1>
+      </body>
+    </Html>
+  )
+}
